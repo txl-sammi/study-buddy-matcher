24f6d64d6d34d752a343c9196659f0ea2d1df481
sammi <txl.sammi@gmail.com>
2023-08-26 16:25:49 +1000

    connects to db, added posts

--- server/index.js
+++ server/index.js
@@ -5,15 +5,18 @@ import dotenv from "dotenv";
 import cors from "cors";
 import flash from "express-flash";
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
 dotenv.config();
+
+app.use('/posts', postRoutes);
 
 // Controls the size of images
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended:true}));
 app.use(cors());
 app.use(flash());
-
 
 app.get("/", (req, res) => {
     res.send("StudyBuddyFinder API");
@@ -22,7 +25,7 @@ app.get("/", (req, res) => {
 const CONNECTION_URL="mongodb+srv://catalyst:Catalyst2023@cluster.12ldr0v.mongodb.net/?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewParser: true, useUnifiedTopology: true})
+mongoose.connect(CONNECTION_URL)
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
